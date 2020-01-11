8349bcc2a6072ef14120fa7d5e0d05322b17acda
Camilla <pantojacamilla@gmail.com>
2020-01-11 12:07:08 -0300

    Adiciona os arquivos do framework

---
+++ app/views/pages/index.php
@@ -0,0 +1,7 @@
+<?php require APPROOT . '/views/inc/header.php'?>
+
+    <h1><?php echo $data['title'];?></h1>
+
+    <p>This is Mary Tech's PHP custom MVC framework</p>
+        
+<?php require APPROOT . '/views/inc/footer.php'?>
